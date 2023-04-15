c6034ceb35ef06bb9c694f78eeda979a98e56309
Angad Singh <angad.singh@imedia24.de>
2023-04-15 22:28:18 +0100

    Add script to deploy contract

--- contracts/GMsV2.sol
+++ contracts/GMsV2.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 import "./GMs.sol";
 
 contract GMsV2 is GMs {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
 import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,6 +13,24 @@ const config: HardhatUserConfig = {
     //     enabled: true,
     //   }
     // }
+  },
+  networks: {
+    hardhat: {},
+    polygon: {
+      chainId: 137,
+      url: "https://polygon-rpc.com/\n",
+      accounts: [process.env.PRIVATE_KEY!!],
+      gasMultiplier: 5,
+      gasPrice: 500_000_000_000,
+      gas: 50_000_000,
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY!!]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
   },
 };
 
